3c4f2b480c185d1b36867edac0e3a54cca3986b4
Sakthi Guna <94789529+IcePrince7@users.noreply.github.com>
2023-07-18 13:54:52 +0530

    Second Commit

--- src/app/Service/guards.ts
+++ src/app/Service/guards.ts
@@ -1,13 +1,1 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
-
-@Injectable()
-
-export class studAllow  implements CanActivate{
-    
-const canActivateTeam: CanActivate =
-(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) =>  {
-        return true
-       }    
-    
-}
+ 
